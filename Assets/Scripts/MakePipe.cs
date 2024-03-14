65f14db864f687fddedae087356469f45d274d4b
dfasdfgs <163072072+dfasdfgs@users.noreply.github.com>
2024-03-14 12:39:06 +0900

    파이프 랜덤

---
+++ Assets/Scripts/MakePipe.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float TimeDiff;
+
+    float timer = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer>TimeDiff)
+        {
+           GameObject newPipe = Instantiate(pipe);
+            newPipe. transform.position = new Vector3(3.83f, Random.Range(-3.35f, 0.81f), 0);
+            timer = 0;
+        }
+    }
+}
